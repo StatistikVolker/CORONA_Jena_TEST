c11650019890cbc0f4f36f65a0abe7102c5b4d2a
Firstname <volker.holzendorf@jena.de>
2020-05-13 10:50:47 +0200

    Code für R plot eingefügt

--- Test repzahl.R
+++ Test repzahl.R
@@ -21,5 +21,31 @@ dftsI_TH <- dftsI %>% filter(county == "SK Leipzig" | Bundesland == "Thüringen"
 dfestR <- dftsI_TH %>% filter(county == "SK Leipzig" )
 
 
-estimate_R(incid = dfestR$i, method = c("si_from_data"),
-           config = make_config(incid = dfestR$i, method = "si_from_data"))
+#------------------------------------------------------------------------------------
+# Reproduktionszahl für 7 Tage Intervalle
+#------------------------------------------------------------------------------------
+T <- length(dfestR$i)
+t_start <- seq(2, T-6) # starting at 2 as conditional on the past observations
+t_end <- t_start + 6 # adding 6 to get 7-day windows as bounds included in window
+# Mit SI Distribution
+estR7 <- estimate_R(dfestR$i,
+                    method="non_parametric_si",
+                    config = make_config(list(
+                      t_start = t_start,
+                      t_end = t_end,
+                      si_distr = dfestR$ip))
+)
+
+#p<-plot(res_parametric_si, legend = FALSE,what = "R", options_R = list(col = "darkred",size = 2));p
+p<-plot(estR7, legend = FALSE,what = "R", options_R = list(col = "darkred",size = 2));p
+p<-plot(estR7, legend = FALSE,what = "incid", options_R = list(col = "darkred",size = 2));p
+
+p + theme_minimal() +
+  guides(color = FALSE, fill = FALSE) +
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_text(size = 20),
+        plot.title = element_text(size = 25,face = "bold")
+  ) +
+  labs(y = "Reproduktionszahl, gesch\u00E4tzt",
+       title = "Reproduktionszahl in Leipzig - 7 Tages Intervall",
+       x = "Tage seit erstem Fall")
